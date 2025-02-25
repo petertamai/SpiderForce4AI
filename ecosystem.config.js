a9e73a651ac283c767133e04a95979d720e4fc3a
pt@petertam.pro <pt@petertam.pro>
2025-02-26 00:04:15 +0700

    Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     exec_mode: 'cluster',
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    max_memory_restart: '2G',
     env: {
       NODE_ENV: 'development',
       PORT: 3004
